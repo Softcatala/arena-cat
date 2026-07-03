d5a2cab600a74a4baf84e3b61e717225fd18c236
Isaac Nicolas <isaacnicolas97@gmail.com>
2026-07-03 11:32:09 +0200

    feat: afegir funcions per la gestio de tokens

---
+++ backend/app/security.py
@@ -0,0 +1,58 @@
+import hmac
+import json
+import base64
+from datetime import datetime, timezone, timedelta
+from app.config import get_settings
+
+def create_token(prompt_id: int, response_a_id: int, response_b_id: int) -> str:
+    settings = get_settings()
+    exp = (datetime.now(timezone.utc) + timedelta(hours=1)).timestamp()
+
+    payload = {
+        'prompt_id': prompt_id,
+        'response_a_id': response_a_id,
+        'response_b_id': response_b_id,
+        'exp' : exp
+    }
+
+    payload_bytes = json.dumps(payload, separators=(',', ':')).encode('utf-8')
+    payload_b64 = base64.urlsafe_b64encode(payload_bytes).decode('utf-8').rstrip("=")
+
+    signature = hmac.new(
+        settings.api_secret_key.encode('utf-8'),
+        payload_bytes,
+        "sha256"    
+    ).digest()
+
+    signature_b64 = base64.urlsafe_b64encode(signature).decode('utf-8').rstrip("=")
+
+    return f"{payload_b64}.{signature_b64}"
+
+def verify_token(token: str) -> dict | None:
+    settings = get_settings()
+
+    try:
+        payload_b64, signature_b64 = token.split(".")
+
+        payload_bytes = base64.urlsafe_b64decode(payload_b64 + "===")
+        signature_provided = base64.urlsafe_b64decode(signature_b64 + "===")
+
+        expected_signature = hmac.new(
+            settings.api_secret_key.encode('utf-8'),
+            payload_bytes,
+            "sha256"
+        ).digest()
+
+        if not hmac.compare_digest(expected_signature, signature_provided):
+            return None
+
+        payload = json.loads(payload_bytes.decode('utf-8'))
+
+        if datetime.now(timezone.utc).timestamp() > payload["exp"]:
+            return None
+
+        return payload
+    
+    except Exception:
+        return None
+
